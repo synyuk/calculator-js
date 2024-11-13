575653db05a8db33e8cc1300b83dee959e788121
Andrey Synyuk <andrey.sinyuk@triogeeks.com>
2024-11-13 18:51:39 +0200

    add eval

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 let display = document.querySelector('.display');
-let buttons = document.querySelectorAll('button.number, button.operator');
+let buttonsNumber = document.querySelectorAll('button.number');
+let buttonsOperetor = document.querySelectorAll('button.operator');
 let reset = document.querySelector('.reset');
+let result = document.querySelector('.result');
+let lastAction = null;
 
-buttons.forEach(item => {
+buttonsNumber.forEach(item => {
     item.addEventListener("click", () => {
-        let num = item.innerHTML;
+        if(lastAction == 'result'){
+            display.value = "0";
+            lastAction = null;
+        }
+        let num = item.innerHTML;        
         if (display.value === "0") {
             display.value = num;
         } else {
@@ -13,10 +20,25 @@ buttons.forEach(item => {
     });    
 });
 
-document.addEventListener("click", () => {
-   
-})
+buttonsOperetor.forEach(item => {
+    item.addEventListener("click", () => {
+        let num = item.value;        
+        if (display.value === "0") {
+            display.value = num;
+        } else {
+            display.value += num;
+        }               
+    });    
+});
 
 reset.addEventListener('click', () => {
     display.value = 0;
-})  
+}) 
+
+result.addEventListener("click", handler);    
+
+function handler(){
+    display.value = eval(display.value);
+    lastAction = 'result';   
+}
+
